57509a8704b92c5c23cecaa44a2ceab7b5a5d112
Daniel Rodriguez <daniel.rodriguez77@educa.madrid.org>
2026-09-17 16:24:30 +0200

    Usuario en mayusculas

--- src/es/ciudadescolar/Programa.java
+++ src/es/ciudadescolar/Programa.java
@@ -3,7 +3,7 @@ import es.ciudadescolar.utils.MessageManager;
 public class Programa {
     public static void main(String[] args) {
         MessageManager mensaje = new MessageManager();
-        String usuario = System.getProperty("user.name");
+        String usuario = System.getProperty("user.name").toUpperCase();
 
         System.out.println(mensaje.getSaludo() + " " + usuario);
            
