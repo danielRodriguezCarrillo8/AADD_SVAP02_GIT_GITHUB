80055d1a3011465f9d891288a10da17db9f9bcc1
Daniel Rodriguez <daniel.rodriguez77@educa.madrid.org>
2026-09-17 16:20:13 +0200

    Añadido usuario al mensaje

--- src/es/ciudadescolar/Programa.java
+++ src/es/ciudadescolar/Programa.java
@@ -3,8 +3,9 @@ import es.ciudadescolar.utils.MessageManager;
 public class Programa {
     public static void main(String[] args) {
         MessageManager mensaje = new MessageManager();
+        String usuario = System.getProperty("user.name");
 
-        System.out.println(mensaje.getSaludo());
+        System.out.println(mensaje.getSaludo() + " " + usuario);
            
     }
 }
