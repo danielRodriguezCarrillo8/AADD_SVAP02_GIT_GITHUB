c84a184a98c3372119a8f0a638d2e7bbc6942ee0
Daniel Rodriguez <daniel.rodriguez77@educa.madrid.org>
2026-09-17 16:01:51 +0200

    Añadido MessageManager

--- src/es/ciudadescolar/Programa.java
+++ src/es/ciudadescolar/Programa.java
@@ -1,10 +1,10 @@
 package es.ciudadescolar;
 import es.ciudadescolar.utils.MessageManager;
-
 public class Programa {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         MessageManager mensaje = new MessageManager();
 
         System.out.println(mensaje.getSaludo());
+           
     }
 }
---
+++ src/es/ciudadescolar/utils/MessageManager.java
@@ -0,0 +1,11 @@
+package es.ciudadescolar.utils;
+
+public class MessageManager {
+
+    private final String saludo = "Empezamos a usar Repositorios de Código...";
+
+    public String getSaludo() {
+        return saludo;
+    }
+
+}
